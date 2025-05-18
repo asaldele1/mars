01a16bbff434b163f8a4814c252bbe7381d224aa
asalde_le1 <57564314+asaldele1@users.noreply.github.com>
2025-05-18 15:08:41 +0300

    Задача "Двойная защита"

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, redirect, render_template, url_for
+
+from forms.login import LoginForm
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "yandexlyceum_secret_key"
@@ -46,6 +48,17 @@ def auto_answer():
 
     return render_template("auto_answer.html", **param)
 
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect("/success")
+    return render_template("login.html", title="Авариный доступ", form=form)
+
+@app.route("/success")
+def success():
+    return render_template("success.html", title="Успех")
 
 if __name__ == "__main__":
     app.run(port=8080)
